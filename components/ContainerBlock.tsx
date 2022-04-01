25390dd4ffbce6b4555a3b48dfe565983768c05c
Facu Murga <fmurga2011@gmail.com>
2022-04-01 11:37:48 -0300

    adding portfolio

---
+++ components/ContainerBlock.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { Footer } from "./Footer";
+import { Navbar } from "./Navbar";
+
+export const ContainerBlock = ({children, ...customMeta}) => {
+    const router = useRouter();
+    const meta = {
+        title: "Facundo Murga - Developer, Systems Engineer",
+        description: "I am developer taking my first steps and looking for an oportunity to prove my skills",
+        image: "/avatar.png",
+        type: "website",
+        ...customMeta,
+    }
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_WEBSITE_URL}${router.asPath}`} />
+        <link rel="canonical" href={`${process.env.NEXT_PUBLIC_WEBSITE_URL}${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Manu Arora" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@mannupaaji" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
+      <main className="dark:bg-gray-800 w-full">
+        <Navbar />
+        <div>{children}</div>
+        <Footer />
+      </main>
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import {ContainerBlock} from "../components/ContainerBlock";
+import {FavouriteProjects} from "../components/FavouriteProjects";
+import {LatestCode} from "../components/LatestCode";
+import {Hero} from "../components/Hero";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <ContainerBlock
+    title="Facu Murga - Developer, Engineer"
+    description="This is my personal portfolio"
+    >
+    <Hero />
+    <FavouriteProjects />
+    <LatestCode />
+  </ContainerBlock>
+  )
+}
+
+export default Home
